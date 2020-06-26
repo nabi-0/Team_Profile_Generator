701782ce796cc88c66f901c42b934365645eda05
Natalia Arias <abram363@hotmail.com>
2020-06-26 10:51:43 -0500

    rearranging folders and files

---
+++ Develop/lib/Manager.js
@@ -0,0 +1,18 @@
+// TODO: Write code to define and export the Engineer class.  
+// HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee.js");
+
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email);
+
+        this.officeNumber = officeNumber;
+        this.role = "Manager";
+    };
+    getOfficeNumber() {
+        return this.officeNumber;
+    }
+};
+
+module.exports = Manager;
